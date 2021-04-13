f8bac9752e6f0e0ef975dd8028b5f8a36393f40a
Benedicte <benedictevikvam@hotmail.com>
2021-04-13 17:12:28 +0200

    🌻 Added redux-persist to persist the cart reducer across sessions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
 
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import userReducer from './user/userReducer';
 import cartReducer from './Cart/cartReducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
